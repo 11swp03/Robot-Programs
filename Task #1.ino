052f4b671336ecef3fe24aa5af0949558fd964ce
11jwp03 <jwp1107@gmail.com>
2022-09-11 16:14:51 -0400

    Tdelay to 1500

--- Task #1.ino
+++ Task #1.ino
@@ -14,7 +14,7 @@ const int speed_MidH = 191;
 const int speed_Max = 255;
 //Variable
 const int FDelay = 3000;
-const int TDelay = 2000;
+const int TDelay = 1500;
 
 //A Motors are Right
 //B Motors are Left
