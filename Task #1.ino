cf0ddd8bd08cfbcf486a173e0ee9e92b3d26e5f0
11jwp03 <11swp03@gmail.com>
2022-09-13 13:44:01 -0400

    added comments for project

--- Task #1.ino
+++ Task #1.ino
@@ -1,5 +1,6 @@
 #include <arduino.h>
 
+//defines which pins do what
 #define PIN_Motor_STBY 3
 #define PIN_Motor_PWMA 5
 #define PIN_Motor_PWMB 6
@@ -7,12 +8,14 @@
 #define PIN_Motor_BIN_1 8
 
 //Motor Speed Presets
-const int speed_Zero = 0;
-const int speed_Min = 64;
-const int speed_MidL = 127;
-const int speed_MidH = 191;
-const int speed_Max = 255;
+//sets motor speed variables
+const int speed_Zero = 0; //0% duty load
+const int speed_Min = 64; //25% duty load
+const int speed_MidL = 127; //50% duty load
+const int speed_MidH = 191; //75% duty load
+const int speed_Max = 255; //100% duty load
 //Variable
+//sets delay variables making it easier to change them all at once
 const int FDelay = 3000; //at speed_Min is approx. 2 feet
 const int TDelay = 1050; //at speed_Min is approx. 90 degrees
 const int HTDelay1 = 1400; //at speed_Min is approx. 315 degrees
@@ -22,14 +25,14 @@ const int HTDelay2 = 480; //at speed_Min is approx. 45 degrees
 //A Motors are Right
 //B Motors are Left
 void setup() {
-    pinMode(PIN_Motor_PWMA, OUTPUT);
+    pinMode(PIN_Motor_PWMA, OUTPUT); 
     
     pinMode(PIN_Motor_AIN_1, OUTPUT);
     pinMode(PIN_Motor_PWMB, OUTPUT);
     pinMode(PIN_Motor_BIN_1, OUTPUT);
     pinMode(PIN_Motor_STBY, OUTPUT);
 
-    digitalWrite(PIN_Motor_STBY,HIGH);
+    digitalWrite(PIN_Motor_STBY,HIGH); //turns motor on
 
     //Move Forward
         digitalWrite(PIN_Motor_AIN_1,HIGH);
