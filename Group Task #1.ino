2d7af9dbbaa6c0e784fd74e5b12d0d2c5d090ff2
11jwp03 <11swp03@gmail.com>
2022-09-20 11:59:22 -0400

    move 2 ft

--- Group Task #1.ino
+++ Group Task #1.ino
@@ -27,6 +27,13 @@ pinMode(PIN_Motor_PWMA, OUTPUT);
     pinMode(PIN_Motor_STBY, OUTPUT);
 
     digitalWrite(PIN_Motor_STBY,HIGH); //turns motor on
+    
+    //Move Forward
+        digitalWrite(PIN_Motor_AIN_1,HIGH);
+        digitalWrite(PIN_Motor_BIN_1,HIGH);
+        analogWrite(PIN_Motor_PWMA,speed_Min);
+        analogWrite(PIN_Motor_PWMB,speed_Min);
+        delay((2 * FDelay));
 }
 
 void loop () {}
