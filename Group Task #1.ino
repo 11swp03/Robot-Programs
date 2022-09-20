b9f49eeeddd48d06cd4541f80f9bb6c86030380c
11jwp03 <11swp03@gmail.com>
2022-09-20 12:02:11 -0400

    move 4 ft

--- Group Task #1.ino
+++ Group Task #1.ino
@@ -55,6 +55,13 @@ pinMode(PIN_Motor_PWMA, OUTPUT);
         analogWrite(PIN_Motor_PWMA,speed_Min);
         analogWrite(PIN_Motor_PWMB,speed_Min);
         delay(TDelay);
+
+        //Move Forward
+        digitalWrite(PIN_Motor_AIN_1,HIGH);
+        digitalWrite(PIN_Motor_BIN_1,HIGH);
+        analogWrite(PIN_Motor_PWMA,speed_Min);
+        analogWrite(PIN_Motor_PWMB,speed_Min);
+        delay((4 * FDelay));
 }
 
 void loop () {}
