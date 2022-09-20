acb4b07112507a37c7fae6f0d46ab7c938cde9a0
11jwp03 <11swp03@gmail.com>
2022-09-20 12:35:28 -0400

    RTDelay to 1200

--- Group Task #1.ino
+++ Group Task #1.ino
@@ -16,7 +16,7 @@ const int speed_Max = 255; //100% duty load
 
 //sets delay variables making it easier to change them all at once
 const int LTDelay = 950; // left 
-const int RTDelay = 1100; //at speed_Min is approx. 90 degrees
+const int RTDelay = 1200; //at speed_Min is approx. 90 degrees
 
 void setup () {
 pinMode(PIN_Motor_PWMA, OUTPUT); 
