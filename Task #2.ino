e51a41f1dcaea1fcf0e6dd3b71d32e4b19520547
11jwp03 <11swp03@gmail.com>
2022-10-03 11:35:13 -0400

    add variables

---
+++ Task #2.ino
@@ -0,0 +1,16 @@
+#include <arduino.h>
+
+//defines which pins do what
+#define PIN_Motor_STBY 3
+#define PIN_Motor_PWMA 5
+#define PIN_Motor_PWMB 6
+#define PIN_Motor_AIN_1 7
+#define PIN_Motor_BIN_1 8
+
+//Motor Speed Presets
+//sets motor speed variables
+const int speed_Zero = 0; //0% duty load
+const int speed_Min = 64; //25% duty load
+const int speed_MidL = 127; //50% duty load
+const int speed_MidH = 191; //75% duty load
+const int speed_Max = 255; //100% duty load
