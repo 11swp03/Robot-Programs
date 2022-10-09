e02235122fbf87001a9aabfc4080dd3ef07238a9
11swp03 <papallo@hartford.edu>
2022-10-09 15:12:05 -0400

    change speed for true condition

--- Group Task #2.ino
+++ Group Task #2.ino
@@ -52,8 +52,8 @@ void loop(){
    if (middleDistance >= 8){
       digitalWrite(PIN_Motor_AIN_1,HIGH);
       digitalWrite(PIN_Motor_BIN_1,HIGH);
-      analogWrite(PIN_Motor_PWMA,speed_Min);
-      analogWrite(PIN_Motor_PWMB,speed_Min);
+      analogWrite(PIN_Motor_PWMA,speed_MidH);
+      analogWrite(PIN_Motor_PWMB,speed_MidH);
    }
    //Slows down otherwise
    else{
