34f1d29c2df73563ea1b107d6f87d292dba7c972
11jwp03 <11swp03@gmail.com>
2022-10-12 11:26:02 -0400

    mid l to h

--- Group Task #2.ino
+++ Group Task #2.ino
@@ -79,7 +79,7 @@ void loop(){
       else {
          digitalWrite(PIN_Motor_AIN_1,HIGH);
          digitalWrite(PIN_Motor_BIN_1,HIGH);
-         analogWrite(PIN_Motor_PWMA,speed_MidL);
+         analogWrite(PIN_Motor_PWMA,speed_MidH);
          analogWrite(PIN_Motor_PWMB,speed_Low);
       }
    }
