5271e3dc89cf38876a1e847370d2bf0f20af7f24
Aiden Kiss <aidenk.7103@gmail.com>
2022-12-01 14:49:12 -0500

    idk this might work

--- aidendoor/aidendoor.ino
+++ aidendoor/aidendoor.ino
@@ -41,35 +41,19 @@ int Distance_test(){
    return (int)Fdistance;
    }
 
-
-void loop(){
-   //move declarations out of the loop
-
-   long inches; //make floating point variable to hold distance
-   pinMode(Trig, OUTPUT);//Tell Trig pin to output sound
-   pinMode(Echo, INPUT);
-   int middleDistance = Distance_test();//runs distance test sub-routine
-   int ButtonState = digitalRead(BUTTON);
-   pinMode(BUTTON, INPUT);
-
-
-   if (ButtonState == 0 && Flag == 0){
-      Flag = 1;
-   }
-   else if (ButtonState == 0 && Flag == 1){
-      Flag = 0;
+int Distance_test2(){
+   digitalWrite(Trig2, LOW);
+   delayMicroseconds(2);
+   digitalWrite(Trig2, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(Trig2, LOW);
+   long Fdistance2 = pulseIn(Echo2, HIGH);
+   Fdistance2 = Fdistance2/148;//converts time delay to distance
+   return (int)Fdistance2;
    }
 
-   if (Flag == 1){
-      digitalWrite(LightO, HIGH);
-      // closes door if distance from cat is greater than 8"
-      if (middleDistance >= 10){
-         analogWrite(PIN_Motor_PWMA,0);
-         analogWrite(PIN_Motor_PWMB,0);
-      }
-      // opens door otherwise
-      else{
-         digitalWrite(PIN_Motor_AIN_1,LOW);
+void openDoor(){
+digitalWrite(PIN_Motor_AIN_1,LOW);
          digitalWrite(PIN_Motor_BIN_1,LOW);
          analogWrite(PIN_Motor_PWMA,127);
          analogWrite(PIN_Motor_PWMB,127);
@@ -82,12 +66,64 @@ void loop(){
          analogWrite(PIN_Motor_PWMA,127);
          analogWrite(PIN_Motor_PWMB,127);
          delay (2800);
+}
+
+void openDoor2(){
+digitalWrite(PIN_Motor_AIN_1,LOW);
+         digitalWrite(PIN_Motor_BIN_1,LOW);
+         analogWrite(PIN_Motor_PWMA,127);
+         analogWrite(PIN_Motor_PWMB,127);
+         delay (3100);
+         analogWrite(PIN_Motor_PWMA,0);
+         analogWrite(PIN_Motor_PWMB,0);
+         delay (6000);
+         digitalWrite(PIN_Motor_AIN_1,HIGH);
+         digitalWrite(PIN_Motor_BIN_1,HIGH);
+         analogWrite(PIN_Motor_PWMA,127);
+         analogWrite(PIN_Motor_PWMB,127);
+         delay (2800);
+}
+
+void loop(){
+   //move declarations out of the loop
+
+   long inches; //make floating point variable to hold distance
+   pinMode(Trig, OUTPUT);//Tell Trig pin to output sound
+   pinMode(Echo, INPUT);
+   int middleDistance = Distance_test();//runs distance test sub-routine
+   int ButtonState = digitalRead(BUTTON);
+   pinMode(BUTTON, INPUT);
+
+
+   //duplicate
+   long inches2; //make floating point variable to hold distance
+   pinMode(Trig2, OUTPUT);//Tell Trig pin to output sound
+   pinMode(Echo2, INPUT);
+   int middleDistance2 = Distance_test();//runs distance test sub-routine
+
+
+   if (ButtonState == 0 && Flag == 0){
+      Flag = 1;
+   }
+   else if (ButtonState == 0 && Flag == 1){
+      Flag = 0;
+   }
+
+   if (Flag == 1){
+      digitalWrite(LightO, HIGH);
+      // closes door if distance from cat is greater than 8"
+      if (middleDistance >= 10 || middleDistance2 >= 8){
+         analogWrite(PIN_Motor_PWMA,0);
+         analogWrite(PIN_Motor_PWMB,0);
+      }
+      // opens door otherwise
+      else{
+         openDoor();
       }
    }
    else {
       analogWrite(PIN_Motor_PWMA,0);
       analogWrite(PIN_Motor_PWMB,0);
       digitalWrite(LightO, LOW);
-
    }
 }
